e5502ec37b97b2556c8b3ece674a14634facb1c3
thiagohrcosta <thiago_henrique86@hotmail.com>
2019-10-20 11:40:09 -0300

    Update RestaurantSolution.sql

--- Data Science/RestaurantProject/RestaurantSolution.sql
+++ Data Science/RestaurantProject/RestaurantSolution.sql
@@ -2,37 +2,46 @@
 
 SELECT * FROM nomnom;
 
-/* The results display the follow columns => name	neighborhood	cuisine	review	price	health  */
 /* ===========================================================================================*/
 
 /* 2. What are the distinct neighborhoods? */
 
 SELECT DISTINCT neighborhood FROM nomnom;
 
-/* The results display : Brooklyn; Midtown; Chinatown; Uptown; Queens; Downtown
 /* ===========================================================================================*/
 
 /* 3. What are the distinct cuisine types? */
 
 SELECT DISTINCT cuisine FROM nomnom;
 
-/* The results display: Steak; Korean; Chinese; Pizza; Ethiopian; Vegetarian; Italian; Japanese; American; Mediterranean; Indian; 
-Soul Food; Mexican  */
+/* ===========================================================================================*/
 
 /* 4. Suppose we would like some Chinese takeout. What are our options? */
 
 SELECT * FROM nomnom
 WHERE cuisine = 'Chinese';
 
-/* There is to many results to insert here */
-
+/* ===========================================================================================*/
 
 /* 5. Return all the restaurants with reviews of 4 and above. */
 
 SELECT * FROM nomnom
 WHERE review >= 4;
 
-/* There is to many results to insert here */
+/* ===========================================================================================*/
 
+/* 6. Suppose Abbi and Ilana want to have a fancy dinner date. Return all the restaurants that are Italian and $$$.
 
+SELECT * FROM nomnom
+WHERE cuisine = 'Italian' 
+AND price = '$$$';
+
+/* ===========================================================================================*/
+
+/* 7. Your coworker Trey can’t remember the exact name of a restaurant he went to but he knows it contains the word ‘meatball’ in it. Can you find it for him using a query? */
+
+SELECT * FROM nomnom
+WHERE name like '%meatball%';
+
+/* ===========================================================================================*/
 
